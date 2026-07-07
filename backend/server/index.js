14d54dc5d7bc191bd3ffd4f94eae8ec52270d26b
Cheryl Moser <cherylmomo@gmail.com>
2026-07-07 14:31:25 -0600

    stubbed some routes

--- backend/server/index.js
+++ backend/server/index.js
@@ -3,50 +3,58 @@ import 'dotenv/config';
 
 const app = express();
 const port = parseInt(process.env.PORT, 10);
-const version = process.env.VERSION;
-const url = process.env.URL+port+"/"+version;
+const url = process.env.URL+port;
 
 app.listen(port, () => {
   console.log(`Server running at ${url}`);
 });
 
-app.get("/v1", (req, res) => {
-  res.send("<h1>The BackEnd (_|_)</h1>");
-});
-
 /* ROUTES WILL BE MOVED FROM THIS PAGE EXCEPT app.listen */
 
-//GET	/api/v1/listings	Search listings
-app.get("/v1/listings", (req, res) => {
-  res.send("to start, this will return all lines from a json file");
+//GET ROOT Backend confirmation
+app.get("/", (req, res) => {
+  res.send("<h1>The BackEnd (_|_)</h1>");
+  res.sendStatus(200);
 });
 
-//GET	/api/v1/listings/:id	Listing details
-app.get("/v1/listings/:id", (req, res) => {
+//GET	/listings	Search listings
+app.get("/listings", (req, res) => {
+  res.send("returns all listings");
+  res.sendStatus(200);
+});
+
+//GET	/listings/:id	Listing details
+app.get("/listings/:id", (req, res) => {
   res.send("retrieves details of listing");
+  res.sendStatus(200);
 });
 
-//POST	/api/v1/listings	Create listing
-app.post("/v1/listings", (req, res) => {
-  console.log("creates a listing");
+//POST	/listings	Create listing
+app.post("/listings", (req, res) => {
+  //res.send("creates a listing");
+  res.sendStatus(201);
 });
 
-// PATCH	/api/v1/listings/:id	Update listing
-app.patch("/v1/listings/:id", (req, res) => {
-  console.log("updates a listing");
+// PATCH	/listings/:id	Update listing
+app.patch("/listings/:id", (req, res) => {
+  res.send("updates a listing");
+  res.sendStatus(200);
 });
 
-// POST	/api/v1/verifications	Submit verification
-app.post("/v1/verifications", (req, res) => {
-  console.log("for submitting verifications");
+// POST	/verifications	Submit verification
+app.post("/verifications", (req, res) => {
+  res.send("for submitting verifications");
+  res.sendStatus(200);
 });
 
-// GET	/api/v1/searches	Retrieve saved searches
-app.get("/v1/searches", (req, res) => {
+// GET	/searches	Retrieve saved searches
+app.get("/searches", (req, res) => {
   res.send("this will return saved searches");
+  res.sendStatus(200);
 });
 
-// POST	/api/v1/messages	Send message
-app.post("/v1/messages", (req, res) => {
-  console.log("for sending messages");
+// POST	/messages	Send message
+app.post("/messages", (req, res) => {
+  res.send("for sending messages");
+  res.sendStatus(200);
 });
